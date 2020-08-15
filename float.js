eba5011cb25e5e4d93d47ae022707c28822eebcc
Long <hoanglongmit@gmail.com>
2020-08-16 06:37:21 +0700

    update

--- float.js
+++ float.js
@@ -4,7 +4,7 @@
 */
 
 
-class Component {
+class Sprite {
     constructor(x, y, width, height) {
         this.x = x
         this.y = y
@@ -12,6 +12,10 @@ class Component {
         this.height = height
 
         this.frames = []
+        this.curFrame = 0
+        this.framePer = 1
+        this._frmPrCountr = 0
+        this.hasTransition = false
     }
     isCollidedW(otherCpnt) {
         let myleft = this.x,
@@ -28,18 +32,18 @@ class Component {
         }
         return isCollided
     }
-    // animated sprite
-    setFrames(imgArr) {
+    setAnim(imgArr, framePer=5) {
         if (!Array.isArray(imgArr)) {
-            return
+            console.error("setAnim() accepts array of Images only")
         }
         for (const img in imgArr) {
             if (!img) {
-                return
+                console.error("Faulty sprite yo")
             }
         }
         this.frames = imgArr
         this.curFrame = 0
+        this.framePer = framePer
     }
     getCurFrame() {
         return this.frames[this.curFrame]
@@ -47,35 +51,47 @@ class Component {
     shiftFrame() {
         if (this.curFrame + 1 >= this.frames.length) {
             this.curFrame = 0
+            if (this.hasTransition) {
+                this.onAnimCycleDone()
+            }
         } else {
             this.curFrame += 1
         }
     }
-    update(ctx) {
+    update() {
+        this._frmPrCountr += 1
+        if (this._frmPrCountr >= this.framePer) {
+            this.shiftFrame()
+            this._frmPrCountr = 0
+        }
+    }
+    draw(ctx) {
         ctx.drawImage(this.getCurFrame(), this.x, this.y, this.width, this.height)
     }
 }
 
-class Character extends Component {
-    constructor(width, height, x, y) {
+class Character extends Sprite {
+    constructor(x, y, width, height) {
         super(x, y, width, height)
         this.velX = 0
         this.velY = 0
         this.accelX = 0
         this.accelY = 0
-        this.status = "red"
+        this.status = "green"
 
-        this.dragFrce = 0.3 // slipperiness
+        this.dragFrce = 0.22 // slipperiness
     }
-    update(ctx) {
+    update() {
+        super.update()
         this.newPos()
         this.x += this.velX
         this.y += this.velY
         this.hitBorder()
-
-        // super.update(ctx)
+    }
+    draw(ctx) {
         ctx.fillStyle = this.status
         ctx.fillRect(this.x, this.y, this.width, this.height)
+        // super.draw(ctx)
     }
     newPos() {
         if (this.velX < this.accelX) {
@@ -113,33 +129,126 @@ class Character extends Component {
     }
 }
 
-class Rock extends Component {
-    constructor(text, x, y) {
+class Rock extends Sprite {
+    constructor(x, y, text, rockPicArr) {
         super(x, y, 250, 150)
         this.text = text
-        this.setFrames([document.getElementById("rock"), document.getElementById("rock2")])
+        this.setAnim(rockPicArr ||
+            [document.getElementById("rock"), document.getElementById("rock2")]
+        )
     }
-    update(ctx) {
-        super.update(ctx)
+    draw(ctx) {
+        super.draw(ctx)
         ctx.font = "32px Arial"
         ctx.fillStyle = "blue"
         ctx.fillText(this.text, this.x + 60, this.y + 70)
     }
 }
 
-class PatternFill extends Component {
-    constructor(width, height) {
+class PatternFill extends Sprite {
+    constructor(width, height, spriteArr) {
         super(0, 0, width, height)
+        this.setAnim(spriteArr, 10)
+    }
+    draw(ctx) {
+        let pat = ctx.createPattern(this.getCurFrame(), 'repeat')
+        ctx.fillStyle = pat
+        ctx.fillRect(0, 0, this.width, this.height)
+    }
+    isCollidedW() {
+        return false
+    }
+}
+
+class Feesh extends Character {
+    spriteArr = {
+        onWater: [],
+        sink: [],
+        toLand: [],
+        onLand: [],
+        toWater: [],
+        float: [],
+    }
+    constructor() {
+        super(20, 120, 30, 30)
+        this.hasTransition = true
+        this.prevAnim = "onWater"
+        this.curAnim = "onWater"
+        this.isCollided = false
+    }
+    onAnimCycleDone() {
+        console.log(this.curAnim);
+        if (this.isCollided) {
+            switch (this.curAnim) {
+                case "onWater":
+                    this.curAnim = "sink"
+                    this.status = "yellow"
+                    break
+                case "sink":
+                    this.curAnim = "toLand"
+                    this.status = "yellow"
+                    break
+                case "toLand":
+                    this.curAnim = "onLand"
+                    this.status = "green"
+                    break
+                case "onLand":
+                    this.curAnim = "onWater"
+                    this.status = "green"
+                    break
+                case "toWater":
+                    this.curAnim = "toLand"
+                    this.status = "yellow"
+                    break
+                case "float":
+                    this.curAnim = "sink"
+                    this.status = "yellow"
+                    break
+            }
+        } else {
+            switch (this.curAnim) {
+                case "onWater":
+                    this.curAnim = "onWater"
+                    this.status = "green"
+                    break
+                case "sink":
+                    this.curAnim = "onWater"
+                    this.status = "green"
+                    break
+                case "toLand":
+                    this.curAnim = "toWater"
+                    this.status = "yellow"
+                    break
+                case "onLand":
+                    this.curAnim = "toWater"
+                    this.status = "yellow"
+                    break
+                case "toWater":
+                    this.curAnim = "float"
+                    this.status = "yellow"
+                    break
+                case "float":
+                    this.curAnim = "onWater"
+                    this.status = "yellow"
+                    break
+            }
+        }
+
+        if (this.curAnim != this.prevAnim) {
+            this.prevAnim = this.curAnim
+            // this.setAnim(Feesh.spriteArr[this.curAnim], 10)
+        }
     }
 }
 
 
+let fsh = new Feesh()
 let myMovingCanvas = {
     canvas: document.getElementById("my-canvas"),
-    elemnts: [ new Character(30, 30, 10, 120) ],
+    elemnts: [],
     start: function () {
-        this.canvas.width = 500
-        this.canvas.height = 600
+        this.canvas.width = window.screen.width
+        this.canvas.height = window.screen.height
         this.context = this.canvas.getContext("2d")
         this.frameNo = 0
         this.interval = setInterval(() => {
@@ -147,102 +256,89 @@ let myMovingCanvas = {
         }, 33) // 30 FPS
 
         this.elemnts = this.elemnts.concat([
-            new Rock("Re calibrate", 100, 140),
-            new Rock("Re position", 200, 240)
+            new PatternFill(this.canvas.width, this.canvas.height, [
+                document.getElementById("gif_test"),
+                document.getElementById("gif_test2")
+            ]),
+            new Rock(100, 140, "Re calibrate"),
+            new Rock(200, 240, "Re position")
         ])
+
+        fsh.setAnim([
+            document.getElementById("gif_test"),
+            document.getElementById("gif_test2")
+        ], 1)
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
     },
     update: function () {
         let ctx = this.context,
-            el = this.elemnts,
-            char = el[0],
+            els = this.elemnts
             isCollided = false
 
         this.clear()
         this.frameNo += 1
-        if ( (this.frameNo / 9) % 1 == 0 ) {
-            // every 9 frames
-            el.forEach(obj => {
-                obj.shiftFrame()
-            })
+        if ((this.frameNo / 5) % 1 == 0) {
+            // every 5 frames
         }
-        for (let i = 1; i < el.length; i++) {
-            el[i].update(ctx)
-            if (el[i].isCollidedW(char)) {
+        els.forEach(el => {
+            el.update()
+            if (el.isCollidedW(fsh)) {
+                // feesh outa water
                 isCollided = true
             }
-        }
-
-        if (isCollided) {
-            char.status = "red"
-        } else {
-            char.status = "blue"
-        }
-        char.update(ctx)
+            el.draw(ctx)
+        })
+        fsh.isCollided = isCollided
+        fsh.update()
+        fsh.draw(ctx)
     }
 }
+
 
 function onKeyPr(e) {
     // console.log(e.key)
-    let fltng = myMovingCanvas.elemnts[0]
-    switch (e.key) {
-        case "s":
-            fltng.accelY = 3
-            break
-        case "w":
-            fltng.accelY = -3
-            break
-        case "d":
-            fltng.accelX = 3
-            break
-        case "a":
-            fltng.accelX = -3
-            break
-        default:
-            fltng.accelX = 0
-            fltng.accelY = 0
+    if (["ArrowDown", "s", "S"].includes(e.key)) {
+        fsh.accelY = 4
+    } else if (["ArrowUp", "w", "W"].includes(e.key)) {
+        fsh.accelY = -4
+    } else if (["ArrowRight", "d", "D"].includes(e.key)) {
+        fsh.accelX = 4
+    } else if (["ArrowLeft", "a", "A"].includes(e.key)) {
+        fsh.accelX = -4
+    } else {
+        fsh.accelX = 0
+        fsh.accelY = 0
     }
 }
 function onRientation(e) {
-    console.log("AAAAAAAAAAAAA")
-    var x = event.beta  // In degree in the range [-180,180]
-    var y = event.gamma // In degree in the range [-90,90]
-  
-    output.innerHTML  = "beta : " + x + "\n"
-    output.innerHTML += "gamma: " + y + "\n"
-  
-    // Because we don't want to have the device upside down
-    // We constrain the x value to the range [-90,90]
-    if (x >  90) { x =  90}
-    if (x < -90) { x = -90}
-  
-    // To make computation easier we shift the range of 
-    // x and y to [0,180]
-    x += 90
-    y += 90
-  
-    // 10 is half the size of the ball
-    // It center the positioning point to the center of the ball
-    ball.style.top  = (maxY*y/180 - 10) + "px"
-    ball.style.left = (maxX*x/180 - 10) + "px"
+    // console.log(e.gamma)
+    fsh.accelX = e.gamma
+    fsh.accelY = e.beta
 }
 
 function main() {
-    if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
-        window.addEventListener('deviceorientation', onRientation) // lmao
-    } else {
-        window.addEventListener("keypress", onKeyPr)
-    }
+    window.addEventListener('deviceorientation', onRientation) // lmao
+    window.addEventListener("keydown", onKeyPr)
     myMovingCanvas.start()
 }
+
 
 let loaded = 0,
     toLoad = 3 // total images
 function doneLoad() {
     loaded += 1
     if (loaded == toLoad) {
-        document.getElementById("load-scr").classList.add("hidden")
+        fadeAway(document.getElementsByClassName("load-scr")[0])
     }
+}
+function appear(elem) {
+    elem.style.display = null
+}
+function fadeAway(elem) {
+    elem.classList.add("hidden")
+    setTimeout(() => {
+        elem.style.display = "none"
+    }, 600)
 }
