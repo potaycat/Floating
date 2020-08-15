3e52adedc1e6f504655ae95c3d465a5dec11628d
Long <hoanglongmit@gmail.com>
2020-08-15 19:42:58 +0700

    updates

--- float.js
+++ float.js
@@ -1,5 +1,6 @@
 /*
-* YO
+* Yo! Code is free for use :D
+* Author: https://twitter.com/Lreeeon
 */
 
 
@@ -10,31 +11,50 @@ class Component {
         this.width = width
         this.height = height
     }
+    isCollidedW(otherCpnt) { // not used
+        let myleft = this.x,
+            myright = this.x + this.width,
+            mytop = this.y,
+            mybottom = this.y + this.height
+        let otherleft = otherCpnt.x,
+            otherright = otherCpnt.x + otherCpnt.width,
+            othertop = otherCpnt.y,
+            otherbottom = otherCpnt.y + otherCpnt.height
+        let isCollided = true
+        if (mybottom < othertop || mytop > otherbottom || myright < otherleft || myleft > otherright) {
+            isCollided = false
+        }
+        return isCollided
+    }
 
-    // crashWith = function(otherobj) {
-    //     let myleft = this.x
-    //     let myright = this.x + this.width
-    //     let mytop = this.y
-    //     let mybottom = this.y + this.height
-
-    //     let otherleft = otherobj.x
-    //     let otherright = otherobj.x + otherobj.width
-    //     let othertop = otherobj.y
-    //     let otherbottom = otherobj.y + otherobj.height
-    //     let crash = true
-    //     if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-    //         crash = false
-    //     }
-    //     return crash
-    // }
+    // animated sprite
+    setFrameArr(imgArr) {
+        if (!Array.isArray(imgArr)) {
+            return
+        }
+        for (const img in imgArr) {
+            if (!img) {
+                return
+            }
+        }
+        this.frameArr = imgArr
+        this.currFrame = 0
+    }
+    getCurFrame() {
+        return this.frameArr[this.currFrame]
+    }
+    shiftFrame() {
+        if (this.currFrame + 1 >= this.frameArr.length) {
+            this.currFrame = 0
+        } else {
+            this.currFrame += 1
+        }
+    }
 }
-
-
 
 class Character extends Component {
     constructor(width, height, x, y) {
         super(x, y, width, height)
-
         this.velX = 0
         this.velY = 0
         this.accelX = 0
@@ -42,14 +62,15 @@ class Character extends Component {
 
         this.dragFrce = 0.3 // slipperiness
     }
-
     update(ctx) {
         this.newPos()
+        this.x += this.velX
+        this.y += this.velY
+        this.hitBorder()
 
         ctx.fillStyle = "red"
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
-
     newPos() {
         if (this.velX < this.accelX) {
             this.velX += this.dragFrce
@@ -63,15 +84,10 @@ class Character extends Component {
         else if (this.velY > this.accelY) {
             this.velY -= this.dragFrce
         }
-
-        this.x += this.velX
-        this.y += this.velY
-        this.hitBorder()
     }
-
     hitBorder() {
-        let bottom = myGameArea.canvas.height - this.height
-        let right = myGameArea.canvas.width - this.width
+        let bottom = myMovingCanvas.canvas.height - this.height
+        let right = myMovingCanvas.canvas.width - this.width
         if (this.y > bottom) {
             this.y = bottom
             this.accelY = 0
@@ -89,88 +105,61 @@ class Character extends Component {
             this.accelX = 0
         }
     }
-
-
 }
 
-
-class Button extends Component {
-    img = document.getElementById("rock")
-
+class Rock extends Component {
     constructor(text, x, y) {
         super(x, y, 250, 150)
-
         this.text = text
+        this.setFrameArr([document.getElementById("rock"), document.getElementById("rock2")])
     }
-
     update(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.getCurFrame(), this.x, this.y, this.width, this.height)
 
         ctx.font = "32px Arial"
         ctx.fillStyle = "blue"
-        ctx.fillText(this.text, this.x + 60, this.y +70)
-
-        // var myGif = GIF();
-        // let patSrc = document.getElementById("gif_test")
-        // ctx.drawImage(patSrc, 10, 30);
-
-        // var myGif = GIF();
-        // myGif.load(patSrc);
-        // var pattern = ctx.createPattern(patSrc, 'repeat');
-        // ctx.fillStyle = pattern;
-        // // ctx.fillRect(0, 0, 100, 800);
-        // ctx.drawImage(myGif, this.x, this.y, this.width, this.height)
+        ctx.fillText(this.text, this.x + 60, this.y + 70)
     }
 }
 
+class PatternFill {
+
+}
 
 
-
-let myGameArea = {
-    canvas: document.createElement("canvas"),
+let myMovingCanvas = {
+    canvas: document.getElementById("myCanvas"),
     start: function () {
         this.canvas.width = 500
         this.canvas.height = 600
         this.context = this.canvas.getContext("2d")
-        document.body.insertBefore(this.canvas, document.body.childNodes[0])
-        document.getElementById("eee").addEventListener("keypress", onKeyPr)
         this.frameNo = 0
-        this.interval = setInterval(updateGameArea, 33) // 30 FPS
+        this.interval = setInterval(() => {
+            this.update()
+        }, 33) // 30 FPS
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
     },
-    loadScene: function () {
-        ctx = this.canvas.getContext("2d")
-        bg = document.getElementById("gif_test")
-        ctx.drawImage(bg, 0, 0, 500, 500)
+    update: function () {
+        this.clear()
+        this.frameNo += 1
+        if ( (this.frameNo / 9) % 1 == 0 ) {
+            // every 9 frames
+            for (i = 0; i < myButtons.length; i += 1) {
+                myButtons[i].shiftFrame()
+            }
+        }
+        ctx = this.context
+        floatingThing.update(ctx)
+        for (i = 0; i < myButtons.length; i += 1) {
+            myButtons[i].update(ctx)
+        }
     }
 }
 
-function updateGameArea() {
-    ctx = myGameArea.context
-
-    function everyinterval(n) {
-        if ((myGameArea.frameNo / n) % 1 == 0) { return true }
-        return false
-    }
-
-    
-    myGameArea.clear()
-    myGameArea.frameNo += 1
-    if (everyinterval(150)) {
-        // every 150 frames
-    }
-
-    myGameArea.loadScene()
-    floatingThing.update(ctx)
-    for (i = 0; i < myButtons.length; i += 1) {
-        // myButtons[i].update(ctx)
-    }
-}
 
 function onKeyPr(e) {
-    console.log(e.key)
     if (e.key == "s") {
         floatingThing.accelY = 3
     }
@@ -188,19 +177,43 @@ function onKeyPr(e) {
         floatingThing.accelY = 0
     }
 }
-
-
-
+function onRientation(e) {
+    console.log("AAAAAAAAAAAAA");
+    var x = event.beta;  // In degree in the range [-180,180]
+    var y = event.gamma; // In degree in the range [-90,90]
+  
+    output.innerHTML  = "beta : " + x + "\n";
+    output.innerHTML += "gamma: " + y + "\n";
+  
+    // Because we don't want to have the device upside down
+    // We constrain the x value to the range [-90,90]
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
+  
+    // To make computation easier we shift the range of 
+    // x and y to [0,180]
+    x += 90;
+    y += 90;
+  
+    // 10 is half the size of the ball
+    // It center the positioning point to the center of the ball
+    ball.style.top  = (maxY*y/180 - 10) + "px";
+    ball.style.left = (maxX*x/180 - 10) + "px";
+}
 
 let floatingThing
-let myButtons = [];
-let reCalBtn
-let rePosBtn
-let aboutBtn
+let myButtons = []
 
 function main() {
     floatingThing = new Character(30, 30, 10, 120)
-    myButtons.push(new Button("Re calibrate", 100, 140))
-    myButtons.push(new Button("Re position", 200, 240))
-    myGameArea.start()
+    myButtons.push(new Rock("Re calibrate", 100, 140))
+    myButtons.push(new Rock("Re position", 200, 240))
+    myButtons.push(new Rock("About", 999, 999))
+
+    if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
+        window.addEventListener('deviceorientation', onRientation); // lmao
+    } else {
+        window.addEventListener("keypress", onKeyPr)
+    }
+    myMovingCanvas.start()
 }
